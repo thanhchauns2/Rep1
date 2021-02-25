400927fedb21323a72da9602a896184578879037
thanhchauns2 <69909023+thanhchauns2@users.noreply.github.com>
2021-02-25 11:25:45 +0700

    Create HoanViNguoc.cpp

---
+++ HoanViNguoc.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a;
+int C[11], F[11];
+void Result()
+{
+	for (int i=1; i <= a; i++) cout<<C[i];
+	cout<<' ';
+}
+void Try(int x)
+{
+	for (int i = a; i >= 1; i--)
+	{
+		if (F[i] == 0) 
+		{
+			C[x] = i;
+			F[i] = 1;
+			if (x == a) Result();
+			else Try(x+1);
+			F[i] = 0;
+		}
+	}
+}
+int main()
+{
+	int N;
+	cin>>N;
+	while(N--)
+	{
+		memset(F,0,sizeof(F));
+		cin>>a;
+		Try(1);
+		cout<<endl;
+	}
+}
