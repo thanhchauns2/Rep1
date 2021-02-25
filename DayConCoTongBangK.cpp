371d2049ee587dad75da211450f8f10eae7dc181
thanhchauns2 <69909023+thanhchauns2@users.noreply.github.com>
2021-02-25 13:19:07 +0700

    Create DayConCoTongBangK.cpp

---
+++ DayConCoTongBangK.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long a,b;
+set<vector<long long> > F;
+void Try(vector<long long> C)
+{
+	if (C.size()==0) return;
+	long long total = 0;
+	for (long long i = 0; i < C.size(); i++) total += C[i];
+	if (total == b) 
+	{
+		F.insert(C);
+		return;
+	}
+	if (total < b) return;
+	vector<long long> K;
+	for (long long i=0; i<C.size(); i++)
+	{
+		K = C;
+		K.erase(K.begin()+i);
+		Try(K);
+	}
+}
+int main()
+{
+	long long N;
+	cin>>N;
+	while(N--)
+	{
+		F.clear();
+		cin>>a>>b;
+		vector<long long> C(a);
+		for (long long i=0; i<a; i++) cin>>C[i];
+		sort(C.begin(), C.end());
+		Try(C);
+		vector<vector<long long> > K(F.begin(), F.end());
+		for (long long i=0; i<K.size(); i++)
+		{
+			cout<<'[';
+			for (long long j=0; j<K[i].size()-1; j++) cout<<K[i][j]<<' ';
+			cout<<K[i][K[i].size()-1]<<']'<<' ';
+		}
+		if (K.size()==0) cout<<-1;
+		cout<<endl;
+	}
+}
