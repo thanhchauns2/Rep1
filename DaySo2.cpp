73cc530f31227639c5ed128d2b009e1f935e9687
thanhchauns2 <69909023+thanhchauns2@users.noreply.github.com>
2021-02-25 11:49:58 +0700

    Create DaySo2.cpp

---
+++ DaySo2.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int N;
+	cin>>N;
+	while(N--)
+	{
+		int a;
+		cin>>a;
+		int C[a][a];
+		for (int i=0; i<a; i++) cin>>C[0][i];
+		for (int i=1; i<a; i++)
+		{
+			for (int j=0; j<a-i; j++) C[i][j] = C[i-1][j] + C[i-1][j+1];
+		}
+		for (int i=a-1; i>=0; i--)
+		{
+			cout<<'[';
+			for (int j=0; j<a-i-1; j++) cout<<C[i][j]<<' ';
+			cout<<C[i][a-i-1]<<']'<<' ';
+		}
+		cout<<endl;
+	}
+}
