349c430b7bd891a9c46d1b7ae886f6558444dfae
thanhchauns2 <69909023+thanhchauns2@users.noreply.github.com>
2021-02-27 16:17:30 +0700

    Create DemCap.cpp

---
+++ DemCap.cpp
@@ -0,0 +1,194 @@
+// Designed by Nguyen Thanh Chau
+// a.k.a Linh's servant - Ken
+// En Taro Adun! - TemplarAssasin a.k.a Zeratul
+
+// Libraries
+
+#include <bits/stdc++.h>
+// #include <boost/multiprecision/cpp_int.hpp>
+// #include <boost/math/constants/constants.hpp>
+// #include <boost/cstdint.hpp>
+
+// Defines
+	
+	// Typedefs
+	
+	#define ll long long
+	#define ull unsigned long long
+	#define pb push_back
+	#define eb emplace_back
+	#define cl(C) C.clear()
+	#define cint cpp_int
+	#define all(C) C.begin(), C.end()
+	#define Begin() int N; cin>>N; while(N--)
+	
+	//Commands
+	
+		// Loops
+		
+		#define FOR(i,a,b) for (int i=a; i<=b; i++)
+		#define FORD(i,a,b) for (int i=a; i>=b; i--)
+		#define FORA(x,C) for (auto x:C)
+		#define WHILE(x,y) while(cin>>x>>y)
+		#define WHILEF(x,y,f) while(f>>x>>y)
+		#define WHILE2(x) while(cin>>x)
+		#define WHILEGL(a) while(getline(cin,a))
+		#define WHILEGLF(a,f) while(getline(f,a))
+		
+		// Get values
+		
+		#define maxOf(C) max_element(C.begin(), C.end()) - C.begin()
+		#define minOf(C) min_element(C.begin(), C.end()) - C.begin()
+		#define ite(y,x,it) y<x>::iterator it
+		
+		// Clear containers
+		
+		#define reset(C) memset(C,0,sizeof(C))
+		#define clearQueue(queue, x) queue = queue<x> ()
+		#define Fill(C,a) fill(C.begin(), C.end(), a)
+		
+		// Customize containers
+		
+		#define SORT(C) sort(C.begin(), C.end())
+		#define SORTD(C,x) sort(C.begin(), C.end(), greater<x>)
+		#define SORTC(C,check) sort(C.begin(), C.end(), check)
+		#define tlw(C) for(int i=0; i<(sizeof(C)/sizeof(C[0])); i++) C[i] = tolower(C[i])
+		#define tup(C) for(int i=0; i<(sizeof(C)/sizeof(C[0])); i++) C[i] = toupper(C[i])
+		#define Merge(C,F,K) merge(C.begin(), C.end(), F.begin(), F.end(), K.begin());
+		#define Unique(C) C.erase(unique(C.begin(), C.end()), C.end())
+		#define CutInHalf(C,F,x,y) y<x> F(C.begin() + C.size()/2 + 1, C.end()); C.resize(C.size()/2)
+		#define revrs(C) reverse(C.begin(, C.end()))
+		
+		// Scan and print containers
+		
+		#define get(C,a) for(int i=0; i<a; i++) cin>>C[i]
+		#define getVector(C) for(int i=0; i<C.size(); i++) cin>>C[i]
+		#define getDeque(C) for(int i=0; i<C.size(); i++) cin>>C[i]
+		#define getSet(C, a, token) for(int i=0; i<a-1; i++) { cin>>token; C.insert(token); } cin>>token; C.insert(token)
+		#define print(C,a) for(int i=0; i<a; i++) cout<<C[i]<<' '
+		#define printVector(C) for(int i=0; i<C.size(); i++) cout<<C[i]<<' '
+		#define printSet(C,x) for (set<x>::iterator it = C.begin(); it!= C.end(); ++it) cout<<*it<<' '
+		#define printDeque(C) for(int i=0; i<C.size(); i++) cout<<C[i]<<' '
+		
+		// Converts between containers
+		
+		#define toSet(C,x) set<x> (C.begin(), C.end())
+		#define toVector(C,x) vector<x> (C.begin(), C.end())
+		#define toQueue(C,x) queue<x> (C.begin(), C.end())
+		#define toDeque(C,x) deque<x> (C.begin(), C.end())
+		#define toList(C,x) list<x> (C.begin(), C.end())
+		#define toMultiset(C,x) multiset(C.begin(), C.end())
+		
+		// Iterators
+		
+		#define pointer(A,x) A<x>::iterator
+		
+	// Containers
+	
+	#define um(x,y) unordered_map<x,y> 
+	#define m(x,y) map<x,y> 
+	#define p(x,y) pair<x,y> 
+	#define us(x) unordered_set<x> 
+	#define vec(x) vector<x> 
+	#define vec2(C,x,y,token) vector<vector<token> > C(x, vector<token> (y))
+	#define pq(x) priority_queue<x>
+	#define reverse_pq(x) priority_queue<x, vector<x>, greater<x> >
+	
+	// System
+	
+	#define faster() ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	#define pause() system("pause");
+
+// Namespaces
+
+using namespace std;
+// using namespace boost::multiprecision;
+// using boost::multiprecision::cpp_dec_float_50;
+
+//===================== Your code starts HERE =====================
+
+//struct ele
+//{
+//	
+//};
+
+bool check(int a, int b)
+{
+	if (a == 0) return false;
+	if (b == 0) return true;
+	if (a == 1) return false;
+	if (b == 1) return true;
+	if (a == 2)
+	{
+		if (b <= 4) return false;
+		return true;
+	}
+	if (a == 3)
+	{
+		if (b == 3) return false;
+		return true;
+	}
+	return (a < b);
+}
+
+int main()
+{
+	faster();
+	Begin()
+	{
+		int a,b;
+		cin>>a>>b;
+		vec(ll) C(a), F(b);
+		int counting = 0;
+		getVector(C);
+		FOR(i,0,b-1)
+		{
+			cin>>F[i];
+			if (F[i]==3) counting++;
+		}
+		SORT(C);
+		SORT(F);
+		// printVector(C);
+		// cout<<endl;
+		// printVector(F);
+		// cout<<endl;
+		int total = 0;
+		int left = 0;
+		int right = 0;
+		while (left < a && C[left] == 0) left++;
+		while (right < b && F[right] == 0)
+		{
+			right++;
+			total += (a - left);
+		}
+		while (left < a && C[left] == 1) left++;
+		while (right < b && F[right] == 1)
+		{
+			right++;
+			total += (a - left);
+		}
+		int save = right;
+		while (left < a && C[left] == 2)
+		{
+			while (right < b && F[right] <= 4) right++;
+			total += (b - right);
+			left++;
+		}
+		right = save;
+		while (left < a && C[left] == 3)
+		{
+			left++;
+			total += (b - right - counting);
+		}
+		while (left < a && right < b)
+		{
+			if (C[left] >= F[right]) right++;
+			else
+			{
+				total += (b - right);
+				left++;
+			}
+		}
+		cout<<total<<endl;
+	}
+}
